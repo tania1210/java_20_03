fc8d3e7195bbd70b2e1381148fa24e1db13c188b
STarsLight1 <t.sluzhynska@gmail.com>
2023-04-06 20:04:08 +0300

    add encapsulation for class Employee

---
+++ homework_3/src/homework_3/Employee.java
@@ -0,0 +1,57 @@
+package homework_3;
+
+public class Employee {
+	private String pib;
+	private String position;
+	private String email;
+	private String phone;
+	private int age;
+	
+	public Employee(String pib, String position, String email, String phone, int age) {
+		this.pib = pib;
+		this.position = position;
+		this.email = email;
+		this.phone = phone;
+		this.age = age;
+	}
+	
+	public void setPib(String pib) {
+		this.pib = pib;
+	}
+	
+	public String getPib() {
+		return pib;
+	}
+	
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	
+	public String getPosition() {
+		return position;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+}
---
+++ homework_3/src/homework_3/Main.java
@@ -0,0 +1,19 @@
+package homework_3;
+
+import java.util.Scanner;
+
+public class Main {
+/*Шеленко Юрій Олексійович
+ *java developer
+ * project@gmail.com
+ * +380964124593
+ * 30
+ * */
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);		
+		Employee Oleg = new Employee(s.next(), s.next(), s.next(), s.next(), s.nextInt());
+
+	}
+
+}
