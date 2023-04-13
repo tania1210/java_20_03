fb91e7c9872cc03d5f34fdd096604a32715521de
STarsLight1 <t.sluzhynska@gmail.com>
2023-04-13 17:30:02 +0300

    method printColor

--- homework_5/src/homework_5/HomeWorkApp.java
+++ homework_5/src/homework_5/HomeWorkApp.java
@@ -1,15 +1,14 @@
 package homework_5;
 
+import java.util.Scanner;
+
 public class HomeWorkApp {
 	
 	public static String printThreeWords(String str) {
 		return str.join("\n", str.split(", "));
 	}
 
-	public static String checkSumSign() {
-		int a = -1;
-		int b = 2;
-		
+	public static String checkSumSign(int a, int b) {		
 		if((a+b) >= 0) {
 			return "Сума позитивна";
 		}else {
@@ -17,10 +16,28 @@ public class HomeWorkApp {
 		}
 	}
 	
+	public static String printColor(int value) {
+		if(value < 1) {
+			return "Червоний";
+		}else if(value > 0 && value < 101) {
+			return "Жовтий";
+		}else {
+			return "Зелений";
+		}
+	}
+	
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int value = s.nextInt();
+		
 		String str = "Orange, Banana, Apple";
+		
+		
 		System.out.println(printThreeWords(str));
-		System.out.println(checkSumSign());
+		System.out.println(checkSumSign(a, b));
+		System.out.println(printColor(value));
 	}
 
 }
