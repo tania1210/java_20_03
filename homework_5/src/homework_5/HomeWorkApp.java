398442ed932397b31b813f510b6a86069b6e567e
STarsLight1 <t.sluzhynska@gmail.com>
2023-04-13 17:23:02 +0300

    method checkSumSign

--- homework_5/src/homework_5/HomeWorkApp.java
+++ homework_5/src/homework_5/HomeWorkApp.java
@@ -6,9 +6,21 @@ public class HomeWorkApp {
 		return str.join("\n", str.split(", "));
 	}
 
+	public static String checkSumSign() {
+		int a = -1;
+		int b = 2;
+		
+		if((a+b) >= 0) {
+			return "Сума позитивна";
+		}else {
+			return "Сума негативна";
+		}
+	}
+	
 	public static void main(String[] args) {
 		String str = "Orange, Banana, Apple";
 		System.out.println(printThreeWords(str));
+		System.out.println(checkSumSign());
 	}
 
 }
