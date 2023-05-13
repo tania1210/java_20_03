343fb70b57e650dc0f355e4104fa8c023c118fde
STarsLight1 <t.sluzhynska@gmail.com>
2023-05-13 14:58:26 +0300

    add fruits in boxes

--- homework_11/src/homework_11/Test.java
+++ homework_11/src/homework_11/Test.java
@@ -4,14 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 //import homework_11.Apple;
-public class Main {
+public class Test {
 
 	public static void main(String[] args) {
 		Fruit fruit = new Fruit(20);
 		
 		Box <Apple> box1 = new Box <> (20);
 		Box <Orange> box2 = new Box <> (20);
-
+//		box1.add("");
+//		box1.add(100);
+//		box1.add(new Apple());
+//		box1.add(new Orange());
+		
+//		System.out.println(app.getOne(0) + "\n" + app.getOne(1) + "\n" + app.getOne(2));
+		
+//		System.out.println(fruit.size());
 		System.out.println(fruit.add(new Apple()) + " size = " + fruit.size());
 		System.out.println(fruit.add(new Orange()) + " size = " + fruit.size());
 		System.out.println(fruit.add(new Orange()) + " size = " + fruit.size());
@@ -19,30 +26,40 @@ public class Main {
 		System.out.println(fruit.add(new Orange()) + " size = " + fruit.size());
 		System.out.println(fruit.add(new Orange()) + " size = " + fruit.size());
 		System.out.println(fruit.add(new Apple()) + " size = " + fruit.size());
-
-		for(int i = 0; i < fruit.size(); i++) {			
+//		
+////		fruit.get(app1.get(0));
+//		System.out.println("\n\n");
+//		Apple obj;
+//		int j = 0;
+		for(int i = 0; i < fruit.size(); i++) {
+			
+			System.out.println("fruit class = " + fruit.get(i).getClass());
+			
 			if((fruit.get(i)) instanceof Apple) {
 				box1.add((Apple) fruit.get(i));
 				System.out.println(true);
+//				System.out.println("object = " + fruit.get(i) + "   i = " + i);
 			} else if((fruit.get(i)) instanceof Orange){
 				box2.add((Orange) fruit.get(i));
 				System.out.println(true);
+//				System.out.println("object = " + fruit.get(i) + "   i = " + i);
 			} else {
 				System.out.println(false);
 			}
+			
+			
+//			System.out.println(fruit.get(i));
 		}
-		
 		System.out.println("\n\n");
 		for(int k = 0; k < box1.size(); k++) {
 			System.out.println("box1 = " + box1.get(k) + "\nk = " + k);
 		}
-		
 		System.out.println("\n\n");
 		for(int k = 0; k < box2.size(); k++) {
 			System.out.println("box2 = " + box2.get(k) + "\nk = " + k);
 		}
 		
-
+//		System.out.println(box.add() + " size = " + fruit.size());
 		
 
 	}
