9122f11f8d4b5e781e2e9a688c5398f4e67d14bb
STarsLight1 <t.sluzhynska@gmail.com>
2023-05-15 20:01:36 +0300

    is done

--- homework_11/src/homework_11/Main.java
+++ homework_11/src/homework_11/Main.java
@@ -13,48 +13,78 @@ public class Main {
 
 	public static void main(String[] args) {
 		Fruit fruit = new Fruit(20);
+		Fruit fruit2 = new Fruit(20);
 		
 		Box <Apple> box1 = new Box <> (20);
 		Box <Orange> box2 = new Box <> (20);
+		Box <Apple> box11 = new Box <> (20);
 
-		System.out.println(fruit.add(new Apple(wApple)) + " size = " + fruit.size());
-		System.out.println(fruit.add(new Orange(wOrange)) + " size = " + fruit.size());
-		System.out.println(fruit.add(new Orange(wOrange)) + " size = " + fruit.size());
-		System.out.println(fruit.add(new Apple(wApple)) + " size = " + fruit.size());
-		System.out.println(fruit.add(new Orange(wOrange)) + " size = " + fruit.size());
-		System.out.println(fruit.add(new Orange(wOrange)) + " size = " + fruit.size());
-		System.out.println(fruit.add(new Apple(wApple)) + " size = " + fruit.size());
-
+		System.out.println(fruit.add(new Apple(wApple)) + " fruit1");
+		System.out.println(fruit.add(new Orange(wOrange)) + " fruit1" );
+		System.out.println(fruit.add(new Orange(wOrange)) + " fruit1");
+		System.out.println(fruit.add(new Apple(wApple)) + " fruit1");
+		System.out.println(fruit.add(new Orange(wOrange)) + " fruit1");
+		System.out.println(fruit.add(new Orange(wOrange)) + " fruit1");
+		System.out.println(fruit.add(new Apple(wApple)) + " fruit1");
+		
+		System.out.println(fruit2.add(new Apple(wApple)) + " fruit2");
+		System.out.println(fruit2.add(new Orange(wOrange)) + " fruit2" );
+		System.out.println(fruit2.add(new Apple(wApple)) + " fruit2");
+		System.out.println(fruit2.add(new Apple(wApple)) + " fruit2");
+		
+		System.out.println("\n");
+		
+		for(int i = 0; i < fruit.size(); i++) {
+			System.out.println("fruit = " + fruit.get(i));
+		}
+		
+		System.out.println("\n");
+		for(int i = 0; i < fruit2.size(); i++) {
+			System.out.println("fruit2 = " + fruit2.get(i));
+		}
+		
+		System.out.println("\n");
+		System.out.println(fruit.addArray(fruit2));
+		
+		for(int i = 0; i < fruit.size(); i++) {
+			System.out.println("new fruit = " + fruit.get(i));
+		}
+		
 		for(int i = 0; i < fruit.size(); i++) {			
 			if((fruit.get(i)) instanceof Apple) {
 				box1.add((Apple) fruit.get(i));
-				System.out.println(true);
+				System.out.println("is adding to box1");
 			} else if((fruit.get(i)) instanceof Orange){
 				box2.add((Orange) fruit.get(i));
-				System.out.println(true);
+				System.out.println("is adding to box2");
 			} else {
 				System.out.println(false);
 			}
 		}
+		System.out.println("\n");
 		
-		System.out.println("\n\n");
+		box11.add(new Apple(wApple));
+
 		for(int k = 0; k < box1.size(); k++) {
 			System.out.println("box1 = " + box1.get(k) + "\nk = " + k);
 		}
 		
-		System.out.println("\n\n");
+		System.out.println("\n");
 		for(int k = 0; k < box2.size(); k++) {
 			System.out.println("box2 = " + box2.get(k) + "\nk = " + k);
 		}
-//		System.out.println("weight = " + box1.getWeight());
-//		System.out.println("get class = " + box1.get(0).getClass());
-//		System.out.println("weight = " + box1.getWeightF());//double
-		System.out.println("\n\n");
-		System.out.println("weight = " + box1.getWeight(box1.get(0)));
-		System.out.println("weight = " + box2.getWeight(box2.get(0)));
 		
-		System.out.println("\n\n");
-		System.out.println("compare: " + box1.compare(box2.sum));
+		System.out.println("\nbox1 after merge: ");
+		box1.merge(box11);
+		
+		for(int k = 0; k < box1.size(); k++) {
+			System.out.println("new box1 :" + box1.get(k));
+		}
+
+		System.out.println("\nweight = " + box1.getWeight(box1.get(0)));
+		System.out.println("weight = " + box2.getWeight(box2.get(0)));
+
+		System.out.println("\ncompare: " + box1.compare(box2.sum));
 		System.out.println("compare: " + box1.compare(box1.sum));
 		
 	}
@@ -113,22 +143,36 @@ public class Main {
 			return size;
 		}
 		
-		public boolean add(Fruit<? extends Fruit> toAdd) {				
-				if(size > fruits.length/2) {
-//					expandArray();
-					return false;
-				}
-				
+		public boolean add(Fruit<? extends Fruit> toAdd) {							
 				for(int i = 0; i < fruits.length; i++) {
 					if(fruits[i] == null) {
 						fruits[i] = toAdd;
 						size++;
-//						sum += getWeight();
 						return true;
 					}
 				}
 				return false;
 		}		
+		
+		public boolean addArray(Fruit toAdd) {	
+			int k = 0;
+			System.out.println("length: " + fruits.length);
+			for(int i = 0; i < fruits.length; i++) {
+				System.out.println("i = " + i + "\nfruits = " + fruits[i]);
+				
+				if(fruits[i] == null) {
+					fruits[i] = toAdd.get(k);
+					size++;
+					k++;
+					if(toAdd.get(k) == null) {
+						return true;
+					}
+
+					
+				}
+			}
+			return false;
+	}	
 		
 		public Fruit<? extends Fruit> get(int index) {
 			return (Fruit<? extends Fruit>) fruits[index];
@@ -150,7 +194,7 @@ public class Main {
 			this.boxes = new Object[count];
 		}
 		
-		public boolean add(T fruit) {			
+		public boolean add(T fruit) {	
 			for(int i = 0; i < boxes.length; i++) {
 				if(boxes[i] == null) {
 					boxes[i] = fruit;
@@ -169,9 +213,7 @@ public class Main {
 			return (T) boxes[index];
 		}
 	
-		public double getWeight(Fruit<? extends Fruit> classType) {
-//			double sum = 0;			
-			
+		public double getWeight(Fruit<? extends Fruit> classType) {			
 			if(classType instanceof Apple) {
 				for(int i = 0; i < size; i++) {				
 					sum += ((Apple) get(i)).getWeight();			
@@ -185,7 +227,6 @@ public class Main {
 			}
 			return sum;
 		}
-
 		
 		public boolean compare(double sum) {
 			if(this.sum == sum) {
@@ -196,7 +237,14 @@ public class Main {
 
 		}
 		
-		
+		public void merge(Box box11) {
+			size = 0;
+			for(int i = 0; i < boxes.length; i++) {
+				boxes[i] = box11.get(i);
+				size++;
+				return;				
+			}			
+		}
 		
 		
 		
