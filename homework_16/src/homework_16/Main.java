8d98966ef0fa1f6365fdc9317d31c86c9ef39ba8
STarsLight1 <t.sluzhynska@gmail.com>
2023-05-25 13:38:23 +0300

    quickSort

--- homework_16/src/homework_16/Main.java
+++ homework_16/src/homework_16/Main.java
@@ -2,8 +2,38 @@ package homework_16;
 
 public class Main {
 
-	public void quickSort() {
-		
+	public static void quickSort(int[] array, int low, int high) {
+         if (low >= high) {
+        	 return;
+         }
+
+         int middle = low + (high - low) / 2;
+         int opora = array[middle];
+
+         int i = low, j = high;
+         while (i <= j) {
+             while (array[i] < opora) {
+                 i++;
+             }
+
+             while (array[j] > opora) {
+                 j--;
+             }
+
+             if (i <= j) {
+                swap(array, i, j);
+                 i++;
+                 j--;
+             }
+         }
+         
+         if (low < j) {
+        	 quickSort(array, low, j);
+         }
+         
+         if (high > i) {
+    	 	quickSort(array, i, high);         
+         }
 	}
 	
 	public static int[] shakerSort(int [] values) {
@@ -32,16 +62,29 @@ public class Main {
 		return values;
 	}
 	
+	private static void swap(int [] array, int i, int j) {
+		int buff = array[i];
+		array[i] = array[j];
+		array[j] = buff;
+	}
+	
 	public static void main(String[] args) {
 		int [] x = {3, 2, 6, -1, 9, 1, -6, 10, 5};
+		
 		for(int i = 0; i < x.length; i++) {
 			System.out.print(x[i] + " ");
 		}
-		System.out.println();
-		x = shakerSort(x);
+		System.out.print("\nshake : ");
+		
+		shakerSort(x);
+		for(int i = 0; i < x.length; i++) {
+			System.out.print(x[i] + " ");
+		}
+		System.out.print("\nquick : ");
+		quickSort(x, 0, x.length-1);
 		for(int i = 0; i < x.length; i++) {
 			System.out.print(x[i] + " ");
 		}
 	}
-
 }
+
